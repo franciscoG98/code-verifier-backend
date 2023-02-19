bc2a5a9ad96bac2aac97b155c6574f9f358f2b3f
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-02-19 14:14:42 -0300

    modified / and /hello routes

--- index.ts
+++ index.ts
@@ -8,16 +8,22 @@ dotenv.config();
 const app: Express = express();
 const port: string | number = process.env.PORT || 8000;
 
-// Define the first Route of APP
 app.get("/", (req: Request, res: Response) => {
-    // Send Hello Wold
-    res.send("Welcome to API Restful: Node + Express + TS + Jest + Swagger + Mongoose");
+    res.status(200).json({
+        data: {
+            message: "Goodbye, world"
+        }
+    })
 });
 
 app.get("/hello", (req: Request, res: Response) => {
-    // Send Hello Wold
-    res.send("Welcome to GET Route: Hello!");
-});
+    const name = req.query.name || "anonimo";
+    res.status(200).json({
+        data: {
+            message: `Hello, ${name}`,
+        }
+    })
+});    
 
 // Execute APP and listen request to PORT
 app.listen(port, () => {
