a117221c45e8dc0557b3b5938742787fd09feafc
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-02-20 12:13:42 -0300

    helloController & HelloRoute & RootRouter done

---
+++ src/routes/HelloRouter.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import { HelloController } from "@/controller/HelloController";
+import { LogInfo } from "@/utils/logger";
+
+// Router from Express
+let helloRouter = express.Router();
+
+// http://localhost:8000/api/hello?name=Pancho/
+helloRouter.route('/')
+    .get(async (req: Request, res: Response) => {
+        
+        // Obtain Query Param
+        let name: any = req?.query?.name;
+        LogInfo(`Query param: ${name}`);
+
+        // Controller Instance to execute method
+        const controller: HelloController = new HelloController();
+
+        // Obtein response
+        const response = await controller.getMessage(name);
+
+        // Send to the client the response
+        return res.send(response);
+    })
+
+export default helloRouter;
